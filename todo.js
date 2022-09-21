d2e7b66de6348406c5506f1adf2da4ffba329af7
antonie-v <114040245+antonie-v@users.noreply.github.com>
2022-09-20 22:37:03 -0700

    First commit

---
+++ todo.js
@@ -0,0 +1,6 @@
+let submitButton = document.getElementById('btnSubmit');
+submitButton.addEventListener('click', printFunction);
+
+function printFunction() {
+    innerText.innerHTML('Printed Text...');
+}
